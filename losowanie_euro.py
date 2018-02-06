9ad158deca9f6da50f6066191260baf78714a365
Mariusz Wysocki <gomez82@wp.pl>
2018-02-06 15:12:52 +0100

    dodano losowanie euro

---
+++ losowanie_euro.py
@@ -0,0 +1,58 @@
+import random
+
+lista=[]
+zmiany=[]
+
+liczbazakladow=input('wpisz liczbe zakladow ')
+liczbazakladow = int(liczbazakladow)
+
+
+for i in range(49):
+    lista.append(i+1)
+
+print('podstawowa lista {}'.format(lista))
+print(len(lista))
+print('obcinam skrajne wartości')
+
+lista=lista[1:-1]
+print('podstawowa lista {}'.format(lista))
+
+def wczytujemy():
+    dowyciecia = []
+    wpisane = input('wpisz wynik poprzedniego losowania: ')
+    dowycieciastr = wpisane.split(',')
+    for i in range(len(dowycieciastr)):
+        dowyciecia.append(int(dowycieciastr[i]))
+    return dowyciecia
+
+dowyciecia= wczytujemy()
+print('do wyciecia')
+print (dowyciecia)
+
+def wycinamy(lista,dowyciecia):
+    for i in range(len(dowyciecia)):
+        for n in range(len(lista)):
+            if dowyciecia[i] == lista[n]:
+                lista.pop(n)
+                break
+    return lista
+
+lista = wycinamy(lista, dowyciecia)
+print(lista)
+
+print ('usuwamy wyniki z przed ostatniego losowania')
+dowyciecia= wczytujemy()
+print ('do wyciecia {}'.format(dowyciecia))
+lista = wycinamy(lista, dowyciecia)
+print('podstawowa lista {}'.format(lista))
+
+print('teraz losujemy 6 liczb z {}'.format(len(lista)))
+
+for i in range(liczbazakladow):
+    losuje  = random.sample(range(0,len(lista)), 6)
+#    print('teraz z listy wycinamy indexy {}'.format(losuje))
+#   print('z lista podstawowa {}'.format(lista))
+    obstawiamy=[]
+    for i in losuje:
+        obstawiamy.append(lista[i])
+    print('trzeba obstawic {}'.format(obstawiamy))
