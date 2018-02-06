3a68bb15fe54997a279b5f52df4e90e8009cbe48
Mariusz Wysocki <gomez82@wp.pl>
2018-02-06 15:44:55 +0100

    zmiany

--- losowanie_euro.py
+++ losowanie_euro.py
@@ -1,33 +1,16 @@
 import random
+import time
 
 lista=[]
 zmiany=[]
 
-liczbazakladow=input('wpisz liczbe zakladow ')
-liczbazakladow = int(liczbazakladow)
-
-
-for i in range(49):
-    lista.append(i+1)
-
-print('podstawowa lista {}'.format(lista))
-print(len(lista))
-print('obcinam skrajne wartości')
-
-lista=lista[1:-1]
-print('podstawowa lista {}'.format(lista))
-
 def wczytujemy():
     dowyciecia = []
-    wpisane = input('wpisz wynik poprzedniego losowania: ')
+    wpisane = input('wpisz wynik losowania: ')
     dowycieciastr = wpisane.split(',')
     for i in range(len(dowycieciastr)):
         dowyciecia.append(int(dowycieciastr[i]))
     return dowyciecia
-
-dowyciecia= wczytujemy()
-print('do wyciecia')
-print (dowyciecia)
 
 def wycinamy(lista,dowyciecia):
     for i in range(len(dowyciecia)):
@@ -37,8 +20,27 @@ def wycinamy(lista,dowyciecia):
                 break
     return lista
 
+liczbazakladow=input('wpisz liczbe zakladow ')
+liczbazakladow = int(liczbazakladow)
+
+
+for i in range(50):
+    lista.append(i+1)
+
+print('podstawowa lista {}'.format(lista))
+print(len(lista))
+print('obcinam skrajne wartości')
+
+lista=lista[1:-1]
+print('podstawowa lista {}'.format(lista))
+
+
+
+print ('usuwamy wyniki z ostatniego losowania')
+dowyciecia= wczytujemy()
+print ('do wyciecia {}'.format(dowyciecia))
 lista = wycinamy(lista, dowyciecia)
-print(lista)
+print('podstawowa lista {}'.format(lista))
 
 print ('usuwamy wyniki z przed ostatniego losowania')
 dowyciecia= wczytujemy()
@@ -46,13 +48,23 @@ print ('do wyciecia {}'.format(dowyciecia))
 lista = wycinamy(lista, dowyciecia)
 print('podstawowa lista {}'.format(lista))
 
-print('teraz losujemy 6 liczb z {}'.format(len(lista)))
+
+print ('usuwamy wyniki z przed przed ostatniego losowania')
+dowyciecia= wczytujemy()
+print ('do wyciecia {}'.format(dowyciecia))
+lista = wycinamy(lista, dowyciecia)
+print('podstawowa lista {}'.format(lista))
+
+
+
+print('teraz losujemy 5 liczb z {}'.format(len(lista)))
 
 for i in range(liczbazakladow):
-    losuje  = random.sample(range(0,len(lista)), 6)
+    losuje  = random.sample(range(0,len(lista)), 5)
 #    print('teraz z listy wycinamy indexy {}'.format(losuje))
 #   print('z lista podstawowa {}'.format(lista))
     obstawiamy=[]
     for i in losuje:
         obstawiamy.append(lista[i])
     print('trzeba obstawic {}'.format(obstawiamy))
+    time.sleep(2)
